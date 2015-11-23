2f59bbcd4b5f3fb14769a59078795acd05822df7
PashaTkachuk <pavlov.tkachuk@gmail.com>
2015-11-24 00:05:37 +0200

    added contact model.

--- db/db_data.sql
+++ db/db_data.sql
@@ -3,3 +3,17 @@ INSERT INTO user_account (username, password, email) VALUES ('admin', '21232f297
 
 --create 'user' user with 'user' password
 INSERT INTO user_account (username, password, email) VALUES ('user', 'ee11cbb19052e40b07aac0ca060c23ee', 'test@mail2.com');
+
+--add contact 'user'
+INSERT INTO contact_list (user_id, contact_id) VALUES (1, 2);
+
+--add contact 'user'
+INSERT INTO contact_list (user_id, contact_id) VALUES (2, 1);
+
+INSERT INTO message (body, sender) VALUES ('Hello Admin!', 2);
+
+INSERT INTO message (body, sender) VALUES ('Hello User!', 1);
+
+INSERT INTO message_recipient (message_id, recipient_id) VALUES (1, 1);
+
+INSERT INTO message_recipient (message_id, recipient_id) VALUES (2, 2);
--- db/db_schema.sql
+++ db/db_schema.sql
@@ -12,6 +12,15 @@ CREATE TABLE IF NOT EXISTS message (
    message_id  serial PRIMARY KEY,
    body text NOT NULL,
    sender int NOT NULL REFERENCES user_account (user_id),
-   recipient int NOT NULL REFERENCES user_account (user_id),
-   created_at timestamp NOT NULL
+   created_at timestamp DEFAULT current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS message_recipient (
+   message_id int NOT NULL REFERENCES message (message_id),
+   recipient_id int NOT NULL REFERENCES user_account (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS contact_list (
+   user_id int NOT NULL REFERENCES user_account (user_id),
+   contact_id int NOT NULL REFERENCES user_account (user_id)
 );
