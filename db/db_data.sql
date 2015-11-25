53c4cfa6ae3f7220a8ce547fa0b1545c96e1bbc4
Pasha <pavlov.tkachuk@gmail.com>
2015-11-25 20:31:50 +0200

    Added messages.

--- db/db_data.sql
+++ db/db_data.sql
@@ -1,8 +1,8 @@
 --create 'admin' user with 'admin' password
-INSERT INTO user_account (username, password, email) VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3', 'test@mail.com');
+INSERT INTO user_account (username, password, email, first_name, avatar) VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3', 'test@mail.com', 'Admin', 'avatar_user_1.jpg');
 
 --create 'user' user with 'user' password
-INSERT INTO user_account (username, password, email) VALUES ('user', 'ee11cbb19052e40b07aac0ca060c23ee', 'test@mail2.com');
+INSERT INTO user_account (username, password, email, first_name, avatar) VALUES ('user', 'ee11cbb19052e40b07aac0ca060c23ee', 'test@mail2.com', 'User', 'avatar_user_2.jpg');
 
 --add contact 'user'
 INSERT INTO contact_list (user_id, contact_id) VALUES (1, 2);
@@ -16,6 +16,6 @@ INSERT INTO dialog_contact (dialog_id, user_id) VALUES (1, 1);
 
 INSERT INTO dialog_contact (dialog_id, user_id) VALUES (1, 2);
 
-INSERT INTO message (body, sender, dialog) VALUES ('Hello Admin!', 2, 1);
+INSERT INTO message (body, sender, dialog, created_at) VALUES ('Hello Admin!', 2, 1, to_timestamp(1448209952));
 
-INSERT INTO message (body, sender, dialog) VALUES ('Hello User!', 1, 1);
+INSERT INTO message (body, sender, dialog, created_at) VALUES ('Hello User!', 1, 1, to_timestamp(1448287343));
--- db/db_schema.sql
+++ db/db_schema.sql
@@ -3,6 +3,11 @@ CREATE TABLE IF NOT EXISTS user_account (
    username varchar(32) NOT NULL,
    password varchar(64) NOT NULL,
    email varchar(64) NOT NULL,
+   first_name varchar(64) DEFAULT NULL,
+   last_name varchar(64) DEFAULT NULL,
+   avatar varchar(128) DEFAULT 'default_avatar.jpg',
+   skype varchar(32) DEFAULT NULL,
+   phone varchar(32) DEFAULT NULL,
    CONSTRAINT unique_username UNIQUE(username),
    CONSTRAINT unique_email UNIQUE(email)
 );
@@ -13,13 +18,12 @@ CREATE TABLE IF NOT EXISTS dialog (
    type varchar(32) NOT NULL
 );
 
-
 CREATE TABLE IF NOT EXISTS message (
    message_id  serial PRIMARY KEY,
    body text NOT NULL,
    sender int NOT NULL REFERENCES user_account (user_id),
    dialog int NOT NULL REFERENCES dialog (dialog_id),
-   created_at timestamp DEFAULT current_timestamp
+   created_at timestamp NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS dialog_contact (
